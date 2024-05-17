5c9a02ec74734aea7911e3b6aff7f685a28bc425
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-05-17 20:54:17 +0530

    Create Solution.cpp

---
+++ May - 2024/17. Find pair given difference/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+  public:
+    int findPair(int n, int x, vector<int> &arr) 
+    {
+        unordered_set<int> seen;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (seen.find(arr[i] - x) != seen.end() || seen.find(arr[i] + x) != seen.end())
+            {
+                return 1;
+            }
+            seen.insert(arr[i]);
+        }
+        return -1;
+    }
+};
