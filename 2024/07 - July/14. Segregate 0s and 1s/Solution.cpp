4441703f8e5a11403cbba7868065a0a7f393416d
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-07-14 19:10:18 +0530

    Create Solution.cpp

---
+++ 2024/07 - July/14. Segregate 0s and 1s/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+  public:
+    void segregate0and1(vector<int> &arr) 
+    {
+        int left = 0, right = arr.size() - 1;
+
+        while (left < right) 
+        {
+            while (arr[left] == 0 && left < right) 
+                left++;
+
+            while (arr[right] == 1 && left < right) 
+                right--;
+
+            if (left < right) 
+            {
+                swap(arr[left], arr[right]);
+                left++;
+                right--;
+            }
+        }
+    }
+};
