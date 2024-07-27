0d91d7e5a828dd9dc0dc89010afdba0426ce26db
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-07-27 21:04:00 +0530

    Create Solution.cpp

---
+++ 2024/07 - July/27. Form a Palindrome/Solution.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+  public:
+    int countMin(string str)
+    {
+        int n = str.length();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int length = 2; length <= n; length++) 
+        {
+            for (int i = 0; i <= n - length; i++) 
+            {
+                int j = i + length - 1;
+                if (str[i] == str[j]) 
+                {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } 
+                else 
+                {
+                    dp[i][j] = min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        return dp[0][n-1];
+    }
+};
