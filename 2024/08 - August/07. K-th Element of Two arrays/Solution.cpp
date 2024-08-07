3cc5a341c408d441620678ea34eb258817c26125
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-08-07 21:51:32 +0530

    Create Solution.cpp

---
+++ 2024/08 - August/07. K-th Element of Two arrays/Solution.cpp
@@ -0,0 +1,10 @@
+class Solution 
+{
+  public:
+    int kthElement(int k, vector<int>& arr1, vector<int>& arr2) 
+    {
+        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+        sort(arr1.begin(), arr1.end());
+        return arr1[k-1];
+    }
+};
