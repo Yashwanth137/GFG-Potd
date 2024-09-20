985ccde7cca161b435d27042ac978539f7baed41
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-09-20 20:13:20 +0530

    Update Solution.cpp

--- 2024/09 - September/20. Facing the Sun/Solution.cpp
+++ 2024/09 - September/20. Facing the Sun/Solution.cpp
@@ -3,6 +3,7 @@ class Solution
   public:
     int countBuildings(vector<int> &height) 
     {
+        if (height.empty()) return 0;
         int res = 1, temp = height[0];
         for(int i=1; i<height.size(); i++)
         {
