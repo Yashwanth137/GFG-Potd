1614e6614e509cd8b83995510deafc231e284832
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-10-28 20:03:27 +0530

    Create Solution.cpp

---
+++ 2024/10 - October/28. Remove duplicates in array/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) 
+    {
+        unordered_set<int> seen;
+        vector<int> result;
+
+        for (int num : arr) 
+        {
+            if (seen.find(num) == seen.end()) 
+            {
+                seen.insert(num);
+                result.push_back(num);
+            }
+        }
+
+        return result;
+    }
+};
