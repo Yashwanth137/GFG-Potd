65563e913eb6a936f664b813448e4c585ed6b21d
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-11-14 01:13:06 +0530

    Create Solution.cpp

---
+++ 2024/11 - November/14. Nearly Sorted/Solution.cpp
@@ -0,0 +1,31 @@
+class Solution 
+{
+  public:
+    void nearlySorted(vector<int>& arr, int k) 
+    {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        
+        vector<int> result;
+        
+        for (int i = 0; i <= k && i < arr.size(); i++) 
+        {
+            minHeap.push(arr[i]);
+        }
+        
+        for (int i = k + 1; i < arr.size(); i++) 
+        {
+            result.push_back(minHeap.top());
+            minHeap.pop();
+            
+            minHeap.push(arr[i]);
+        }
+        
+        while (!minHeap.empty()) 
+        {
+            result.push_back(minHeap.top());
+            minHeap.pop();
+        }
+        
+        arr = result;
+    }
+};
