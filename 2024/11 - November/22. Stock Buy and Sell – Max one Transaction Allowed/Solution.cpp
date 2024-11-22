c82801c89c3bd4628e57c5d9e2958d4e4957f464
Yash <75321458+Yashwanth137@users.noreply.github.com>
2024-11-22 19:09:28 +0530

    Create Solution.cpp

---
+++ 2024/11 - November/22. Stock Buy and Sell – Max one Transaction Allowed/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int maximumProfit(vector<int>& prices) 
+    {
+        if (prices.size() < 2) return 0;
+
+        int min_price = prices[0];
+        int max_profit = 0;
+
+        for (int i = 1; i < prices.size(); i++) 
+        {
+            max_profit = max(max_profit, prices[i] - min_price);
+            min_price = min(min_price, prices[i]);
+        }
+
+        return max_profit;
+    }
+};
