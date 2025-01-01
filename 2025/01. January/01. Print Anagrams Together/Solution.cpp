8ba9401b8abe5967fc00428b4015f0e1daeddb01
Yash <75321458+Yashwanth137@users.noreply.github.com>
2025-01-01 22:40:29 +0530

    Create Solution.cpp

---
+++ 2025/01. January/01. Print Anagrams Together/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& words) {
+        unordered_map<string, vector<string>> anagramGroups;
+
+        for (string& word : words) {
+            string key = word;
+            sort(key.begin(), key.end());
+            anagramGroups[key].push_back(word);
+        }
+        
+        vector<vector<string>> groupedAnagrams;
+        for (auto& group : anagramGroups) {
+            groupedAnagrams.push_back(group.second);
+        }
+        
+        return groupedAnagrams;
+    }
+};
