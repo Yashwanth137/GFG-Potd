0db629b8b41308fdb70bb157620011f87cbc4b80
Yash <75321458+Yashwanth137@users.noreply.github.com>
2025-02-03 21:10:37 +0530

    Create Solution.cpp

---
+++ 2025/02. February/03. Height of Binary Tree/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+  public:
+    int height(Node* node) 
+    {
+        if (node == NULL) 
+        {
+            return -1;
+        }
+        
+        return 1 + max(height(node->left), height(node->right));
+    }
+};
