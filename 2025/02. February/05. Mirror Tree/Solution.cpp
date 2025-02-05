99fe7eef7141fea76edab2b061571182354fe05a
Yash <75321458+Yashwanth137@users.noreply.github.com>
2025-02-05 19:35:33 +0530

    Create Solution.cpp

---
+++ 2025/02. February/05. Mirror Tree/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+  public:
+    void mirror(Node* node) 
+    {
+        if (node == NULL) return;
+        
+        swap(node->left, node->right);
+        
+        mirror(node->left);
+        mirror(node->right);
+    }
+};
