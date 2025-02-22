31c1decab456b831e0f5fbcd2b23e8549c165f63
Yash <75321458+Yashwanth137@users.noreply.github.com>
2025-02-22 22:53:27 +0530

    Create Solution.cpp

---
+++ 2025/02. February/22. Longest Valid Parenthesis/Solution.cpp
@@ -0,0 +1,32 @@
+class Solution 
+{
+  public:
+    int maxLength(string& s) 
+    {
+        stack<int> stk;
+        stk.push(-1);
+        int res = 0;
+        
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s[i] == '(')
+            {
+                stk.push(i);
+            }
+            else
+            {
+                stk.pop();
+                if(stk.empty())
+                {
+                    stk.push(i);
+                }
+                else
+                {
+                    res = max(res, i - stk.top());
+                }
+            }
+        }
+        
+        return res;
+    }
+};
